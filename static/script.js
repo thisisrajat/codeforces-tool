4fde21d42c4acb66efefc4b776415c9d5431afde
Rajat Jain <rajat.rj10@gmail.com>
2015-01-26 20:18:53 +0530

    append html to div

--- static/script.js
+++ static/script.js
@@ -1,17 +1,31 @@
-var x = new XMLHttpRequest;
+var request = new XMLHttpRequest;
+
+window.onload = function() {
+  document.getElementById('url').style.display = 'none';
+};
 
 function find() {
+  document.getElementById('url').style.display = 'none';
   var username = document.getElementById('username').value;
   var contest = document.getElementById('contest').value;
   var problem = document.getElementById('problem').value;
   var url = '/get_solution?user=' + username + '&contest=' + contest + '&problem=' + problem
-
-  x.open('GET', url, true);
-  x.onreadystatechange = function() {
-    if(x.readyState == 4 && x.status == 200) {
-      document.getElementById('url').href = x.responseText;
-      document.getElementById('url').innerHTML = x.responseText; 
+  request.open('GET', url, true);
+  
+  request.onreadystatechange = function() {
+    if(request.readyState == 4 && request.status == 200) {
+      var data = JSON.parse(request.responseText);
+      document.getElementById('url').style.display = 'block';
+      if(data.status === '!ok') {
+        document.getElementById('url').className = 'container alert alert-danger';
+        var html = "<a href = '#' class = 'red url-center'>" + data['url'] + "</a>";
+        document.getElementById('url').innerHTML = html;
+      } else {
+        document.getElementById('url').className = 'container alert alert-success';
+        var html = "<a href = '" + data['url'] + "' class='green url-center'>" + data['url'] + "</a>"
+        document.getElementById('url').innerHTML = html;
+      }
     }
   }
-  x.send();
+  request.send();
 }
