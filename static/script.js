b3de306ad80dfdb31e78be9f64f734c3ba0cb21e
Rajat Jain <rajat.rj10@gmail.com>
2015-01-27 21:22:01 +0530

    New append to wrapper mode

--- static/script.js
+++ static/script.js
@@ -1,31 +1,47 @@
-var request = new XMLHttpRequest;
-
 window.onload = function() {
-  document.getElementById('url').style.display = 'none';
+  document.getElementById('wrapper').innerHTML = '';
 };
 
-function find() {
-  document.getElementById('url').style.display = 'none';
-  var username = document.getElementById('username').value;
+function get() {
+  document.getElementById('wrapper').innerHTML = '';
+  var usersList = document.getElementById('username').value;
   var contest = document.getElementById('contest').value;
   var problem = document.getElementById('problem').value;
-  var url = '/get_solution?user=' + username + '&contest=' + contest + '&problem=' + problem
+  usersList = usersList.split('+');
+
+  for(var i = 0; i < usersList.length; i++) {
+    findSolutionId('/get_solution?user=' + usersList[i] + '&contest=' + contest + '&problem=' + problem);
+  }
+}
+
+function findSolutionId(url) {
+  var request = new XMLHttpRequest();
   request.open('GET', url, true);
-  
   request.onreadystatechange = function() {
     if(request.readyState == 4 && request.status == 200) {
-      var data = JSON.parse(request.responseText);
-      document.getElementById('url').style.display = 'block';
-      if(data.status === '!ok') {
-        document.getElementById('url').className = 'container alert alert-danger';
-        var html = "<a href = '#' class = 'red url-center'>" + data['url'] + "</a>";
-        document.getElementById('url').innerHTML = html;
-      } else {
-        document.getElementById('url').className = 'container alert alert-success';
-        var html = "<a href = '" + data['url'] + "' class='green url-center'>" + data['url'] + "</a>"
-        document.getElementById('url').innerHTML = html;
+      var response = JSON.parse(request.responseText);
+      if(response.status === '!ok') {
+        var html = htmlToAppend('alert-danger', 'red', response.data, response.whose, response.error);
+        document.getElementById('wrapper').innerHTML += html;
+      }
+      else {
+        var html = htmlToAppend('alert-success', 'green', response.data, response.whose, response.error);
+        document.getElementById('wrapper').innerHTML += html;
       }
     }
   }
   request.send();
+}
+
+function htmlToAppend(alertType, colorType, url, whose, error) {
+  var html = "<div role = 'alert' class = 'alert " + alertType + " urlbox'>";
+  html += "<a class = '" + colorType + "' href = '" + url + "'>";
+  html += whose;
+  if(error !== '') {
+    html += ": " + error;
+  } else {
+    html += "'s solution :)"
+  }
+  html += "</a></div>"
+  return html;
 }
